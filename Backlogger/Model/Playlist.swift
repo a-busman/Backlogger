cceff5475fa4455149dded5d07c9b93a53285fed
Alex Busman <abusman@taser.com>
2017-05-02 23:15:39 -0700

    Start Playlist work.

---
+++ Backlogger/Model/Playlist.swift
@@ -0,0 +1,25 @@
+//
+//  Playlist.swift
+//  Backlogger
+//
+//  Created by Alex Busman on 5/2/17.
+//  Copyright © 2017 Alex Busman. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import Realm
+
+class Playlist: Object {
+    required init() {
+        super.init()
+    }
+    
+    required init(realm: RLMRealm, schema: RLMObjectSchema) {
+        super.init(realm: realm, schema: schema)
+    }
+    
+    required init(value: Any, schema: RLMSchema) {
+        super.init(value: value, schema: schema)
+    }
+}
